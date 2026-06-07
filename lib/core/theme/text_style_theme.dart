3ea4f502690e7451a0ee68f597e6b6345a93e53a
Your Name <you@example.com>
2026-06-07 13:51:28 +0300

    create profile

--- lib/core/theme/text_style_theme.dart
+++ lib/core/theme/text_style_theme.dart
@@ -3,43 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyleTheme {
-  static final TextTheme textThemeLight = TextTheme(
-  );
+  static final TextTheme textThemeLight = TextTheme();
   static final TextTheme textThemeDark = TextTheme(
     displayLarge: TextStyle(
       fontWeight: FontWeight.bold,
       color: AppColors.backgroundLight,
       fontSize: 22.sp,
     ),
-    displayMedium: TextStyle(
-      color: AppColors.grey,
-      fontSize: 14.sp,
-    ),
+    displayMedium: TextStyle(color: AppColors.grey, fontSize: 14.sp),
     headlineLarge: TextStyle(
       fontWeight: FontWeight.bold,
       color: AppColors.secondary,
       fontSize: 36.sp,
     ),
-    headlineSmall: TextStyle(
-      color: AppColors.backgroundDark,
-      fontSize: 18.sp,
-    ),
-    bodySmall: TextStyle(
-      fontSize: 12.sp,
+    headlineSmall: TextStyle(color: AppColors.backgroundDark, fontSize: 18.sp),
+    bodySmall: TextStyle(fontSize: 12.sp, color: AppColors.textAuth),
+    displaySmall: TextStyle(fontSize: 16.sp, color: AppColors.textFieldFont),
+    titleLarge: TextStyle(
+      fontWeight: FontWeight.w900,
+      fontSize: 26.sp,
       color: AppColors.textAuth,
     ),
-    displaySmall: TextStyle(
-      fontSize:16.sp,
-      color: AppColors.textFieldFont
+    titleMedium: TextStyle(
+      fontWeight: FontWeight.w900,
+      fontSize: 22.sp,
+      color: AppColors.backgroundLight,
     ),
-titleLarge:TextStyle(
-  fontWeight: FontWeight.w900,
-  fontSize: 26.sp,
-  color: AppColors.textAuth
-),
-    titleMedium: TextStyle(fontWeight: FontWeight.w900,
-        fontSize: 22.sp,
-        color: AppColors.backgroundLight),
-
   );
 }
--- lib/features/profile/presentation/pages/profile_screen.dart
+++ lib/features/profile/presentation/pages/profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:car_app/core/constant/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -6,7 +8,40 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: SizedBox(height: 30.h)),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Container(
+                  height: 132.h,
+                  width: 132.w,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      colors: [Color(0xffFF5722), Color(0xff333535)],
+                    ),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset("assets/image/profile/personal.png"),
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                Text(
+                  "Alex Sterling",
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                    color: AppColors.backgroundLight,
+                  ),
+                ),
 
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
